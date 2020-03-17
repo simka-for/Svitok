652c5cec8a901d8aafb1f80400c2845f31d31d8a
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-03-17 15:36:31 +0300

    add UI Inteface +admin role + upload files to server

--- src/main/java/com/example/svitok/controller/MainController.java
+++ src/main/java/com/example/svitok/controller/MainController.java
@@ -4,14 +4,20 @@ import com.example.svitok.domain.Message;
 import com.example.svitok.domain.User;
 import com.example.svitok.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
@@ -19,43 +25,59 @@ public class MainController {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam(required = false) String filter, Model model){
 
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+
+        if(filter != null && !filter.isEmpty() ){
+            messages = messageRepository.findByTag("#" + filter);
+        }else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
-    @PostMapping("main")
+    @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model){
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        Message message = new Message(text,"#" + tag, user);
 
-        Message message = new Message(text, tag, user);
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+            message.setFilename(resultFilename);
+        }
+
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
-        return "main";
-    }
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-
-        Iterable<Message> messageList;
-        if(filter != null && !filter.isEmpty() ){
-            messageList = messageRepository.findByTag(filter);
-        }else {
-            messageList = messageRepository.findAll();
-        }
-        model.put("messages", messageList);
 
         return "main";
     }
+
 
 }
